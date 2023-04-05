9c5a3a621b5cae7040e6afd04c60471e1e06d618
dhuff <dallinhuff@gmail.com>
2023-04-05 10:23:31 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Hero from './components/Hero.vue'
+const print = () => console.log('hello')
+</script>
+
+<template>
+  <Hero title="Hi there" body="A wall of text" button-text="word" @action="print"/>
+  <Hero title="Hi there" body="a bigger wall of text" button-text="hi"/>
+  <Hero title="Hi you" body="not much to see here" button-text="work"/>
+  <Hero title="bruv" body="right on" button-text="wild"/>
+</template>
