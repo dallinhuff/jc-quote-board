65f031237e92d20fc18c1af8b48e47b67601ceab
dhuff <dallinhuff@gmail.com>
2023-04-05 10:49:07 -0600

    add prettier & husky lint-staged hook

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-defineProps<{title: string, body: string, buttonText: string}>()
+defineProps<{ title: string; body: string; buttonText: string }>();
 </script>
 
 <template>
   <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">{{title}}</h1>
-        <p class="py-6">{{body}}</p>
-        <button @click="$emit('action')" class="btn btn-primary">{{ buttonText }}</button>
+        <h1 class="text-5xl font-bold">{{ title }}</h1>
+        <p class="py-6">{{ body }}</p>
+        <button @click="$emit('action')" class="btn btn-primary">
+          {{ buttonText }}
+        </button>
       </div>
     </div>
   </div>
